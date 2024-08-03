702a13f719935a5b648ce0cc71605566d2f3e80b
stephendyoungjr <stephenyoung.np@outlook.com>
2024-08-03 01:39:36 -0400

    render package

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,6 +8,7 @@ const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
+
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
