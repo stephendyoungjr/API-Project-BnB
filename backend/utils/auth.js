f902e920123aad4dbc93012c430c64dc35604891
stephendyoungjr <stephenyoung.np@outlook.com>
2024-08-26 19:41:08 -0400

    updates

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -10,17 +10,17 @@ const setTokenCookie = (res, user) => {
     email: user.email,
     username: user.username,
   };
-  
+
   const token = jwt.sign(
     { data: safeUser },
     secret,
-    { expiresIn: String(expiresIn) } 
+    { expiresIn: Number(expiresIn) } 
   );
 
   const isProduction = process.env.NODE_ENV === "production";
 
   res.cookie('token', token, {
-    maxAge: parseInt(expiresIn) * 1000, 
+    maxAge: Number(expiresIn) * 1000, 
     httpOnly: true,
     secure: isProduction,
     sameSite: isProduction && "Lax"
@@ -28,42 +28,3 @@ const setTokenCookie = (res, user) => {
 
   return token;
 };
-
-const restoreUser = (req, res, next) => {
-  const { token } = req.cookies;
-  req.user = null;
-
-  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
-    if (err) {
-      return next();
-    }
-
-    try {
-      const { id } = jwtPayload.data;
-      req.user = await User.findByPk(id, {
-        attributes: {
-          include: ['email', 'createdAt', 'updatedAt']
-        }
-      });
-    } catch (e) {
-      res.clearCookie('token');
-      return next();
-    }
-
-    if (!req.user) res.clearCookie('token');
-
-    return next();
-  });
-};
-
-const requireAuth = function (req, _res, next) {
-  if (req.user) return next();
-
-  const err = new Error('Authentication required');
-  err.title = 'Authentication required';
-  err.errors = { message: 'Authentication required' };
-  err.status = 401;
-  return next(err);
-};
-
-module.exports = { setTokenCookie, restoreUser, requireAuth };
