fff1c93f53483abeba731d8fe594bece4f4838f9
stephendyoungjr <stephenyoung.np@outlook.com>
2024-08-27 19:41:30 -0400

    reviewImage drop

--- backend/db/migrations/20240827192007-create-review-image.js
+++ backend/db/migrations/20240827192007-create-review-image.js
@@ -10,22 +10,32 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       reviewId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Reviews', // Name of the table you're linking to
+          key: 'id' // Primary key in the Reviews table
+        },
+        onDelete: 'CASCADE', // Ensure that if a review is deleted, its images are also deleted
+        allowNull: false
       },
       url: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     });
   },
   async down(queryInterface, Sequelize) {
+    // Make sure to drop the foreign key constraint first if it exists
     await queryInterface.dropTable('ReviewImages');
   }
 };
