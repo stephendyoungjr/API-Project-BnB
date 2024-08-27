817791e520bc5f17db2fd9f41a8acd988f5f2f33
stephendyoungjr <stephenyoung.np@outlook.com>
2024-08-27 19:49:52 -0400

    sql

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { sequelize } = require('./db/models');
 
-console.log('Config Path:', path.resolve('backend', 'config', 'database.js'));
+console.log('Config Path:', path.resolve('config', 'database.js'));
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
