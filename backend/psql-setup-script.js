724b467bc705e6a223a8f4afe3ad6203aaea0148
stephendyoungjr <stephenyoung.np@outlook.com>
2024-08-27 17:45:38 -0400

    Fix: Add path import in psql-setup-script.js

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,8 +1,7 @@
+const path = require('path');
 const { sequelize } = require('./db/models');
 
-
 console.log('Config Path:', path.resolve('backend', 'config', 'database.js'));
-
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
