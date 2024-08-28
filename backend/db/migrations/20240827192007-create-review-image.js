5270c438db8c2c61a5ec17b2ac3980e531043cd6
stephendyoungjr <stephenyoung.np@outlook.com>
2024-08-28 16:04:18 -0400

    checking reviewimage.js

--- backend/db/migrations/20240827192007-create-review-image.js
+++ backend/db/migrations/20240827192007-create-review-image.js
@@ -8,35 +8,23 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Reviews', {
+    await queryInterface.createTable('ReviewImages', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      spotId: {
+      reviewId: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Spots'
+          model: 'Reviews'
         },
         onDelete: 'CASCADE'
       },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users'
-        },
-        onDelete: 'CASCADE'
-      },
-      review: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      stars: {
-        type: Sequelize.INTEGER,
+      url: {
+        type: Sequelize.STRING,
         allowNull: false
       },
       createdAt: {
@@ -50,16 +38,10 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     }, options);
-
-    options.tableName = 'Reviews';
-    await queryInterface.addIndex(options, ['spotId', 'userId'], {
-      unique: true,
-      name: 'idx_reviews_spotId_userId'
-    })
   },
+
   async down(queryInterface, Sequelize) {
-    options.tableName = 'Reviews';
-    await queryInterface.removeIndex(options, 'idx_reviews_spotId_userId');
-    return await queryInterface.dropTable(options);
+    options.tableName = 'ReviewImages';
+    await queryInterface.dropTable(options);
   }
 };
