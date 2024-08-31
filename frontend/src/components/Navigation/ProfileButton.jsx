161ad0c8452197125dcc2452557382244495fdf6
stephendyoungjr <stephenyoung.np@outlook.com>
2024-08-31 08:15:34 -0400

    modal handle

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,93 +1,3 @@
-// import { useEffect, useState, useRef } from "react";
-// import { useDispatch } from "react-redux";
-// import { FaCircleUser } from "react-icons/fa6";
-// import { IoMenu } from "react-icons/io5";
-// import React from 'react';
-
-// import { logout } from "../../store/session";
-// import OpenModalMenuItem from './OpenModalMenuItem';
-// import LoginFormModal from '../LoginFormModal'; 
-// import SignupFormModal from "../SignupFormModal";
-// import { useNavigate } from "react-router-dom";
-
-// const ProfileButton = ({user}) => {
-//     const dispatch =  useDispatch();
-//     const navigate = useNavigate();
-//     const [showMenu, setShowMenu] = useState(false);
-//     const ulRef = useRef(); // Ref for the dropdown menu
-
-//     const toggleMenu = (e) => {
-//         e.stopPropagation();
-//         setShowMenu(!showMenu); // Toggle the menu visibility
-//     };
-
-//     useEffect(() => {
-//         if (!showMenu) return; // If the menu is not shown, do nothing
-
-//         // Function to close the menu if a click is detected outside of it
-//         const closeMenu = (e) => {
-//             if (ulRef.current && !ulRef.current.contains(e.target)) {
-//                 setShowMenu(false); // Close the menu
-//             }
-//         };
-
-//         document.addEventListener('click', closeMenu);
-
-//         // Cleanup the event listener when the component is unmounted or the menu is closed
-//         return () => document.removeEventListener('click', closeMenu);
-//     }, [showMenu]);
-
-//     const closeMenu = () => setShowMenu(false);
-
-//     const logoutClick = (e) => {
-//         e.preventDefault();
-//         dispatch(logout());
-//         closeMenu();
-//         navigate('/');
-//     };
-
-//     const dropdownClasses = "profile-dropdown" + (showMenu ? "" : " hidden");
-
-//     return (
-//         <div>
-//             <button id="toggle-menu-button" onClick={toggleMenu}>
-//                 <IoMenu />
-//                 <FaCircleUser />
-//             </button>
-//             <ul className={dropdownClasses} ref={ulRef}> {/* Attach the ref to the dropdown menu */}
-//                 {user ? (
-//                     <>
-//                         <li>Hello, {user.firstName}</li>
-//                         <li>{user.email}</li>
-//                         <hr></hr>
-//                         <li onClick={() => navigate('/spots/current')}>Manage Spots</li>
-//                         <hr></hr>
-//                         <li id="logout">
-//                             <button onClick={logoutClick}>Log Out</button>
-//                         </li>
-//                     </>
-//                 ) : (
-//                     <>
-//                         <OpenModalMenuItem
-//                             itemText="Sign Up"
-//                             onItemClick={closeMenu}
-//                             modalComponent={<SignupFormModal />}
-//                         />
-//                         <OpenModalMenuItem
-//                             itemText="Log In"
-//                             onItemClick={closeMenu}
-//                             modalComponent={<LoginFormModal />}
-//                         />
-//                     </>
-//                 )}
-//             </ul>
-//         </div>
-//     );
-// }
-
-// export default ProfileButton;
-
-
 import { useEffect, useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { FaCircleUser } from "react-icons/fa6";
@@ -99,8 +9,8 @@ import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from "../SignupFormModal";
 import { useNavigate } from "react-router-dom";
 
-const ProfileButton = ({user}) => {
-    const dispatch =  useDispatch();
+const ProfileButton = ({ user }) => {
+    const dispatch = useDispatch();
     const navigate = useNavigate();
     const [showMenu, setShowMenu] = useState(false);
     const ulRef = useRef(); // Ref for the dropdown menu
@@ -172,6 +82,95 @@ const ProfileButton = ({user}) => {
             </ul>
         </div>
     );
-}
+};
 
 export default ProfileButton;
+
+
+// import { useEffect, useState, useRef } from "react";
+// import { useDispatch } from "react-redux";
+// import { FaCircleUser } from "react-icons/fa6";
+// import { IoMenu } from "react-icons/io5";
+// import React from 'react';
+// import { logout } from "../../store/session";
+// import OpenModalMenuItem from './OpenModalMenuItem';
+// import LoginFormModal from '../LoginFormModal'; 
+// import SignupFormModal from "../SignupFormModal";
+// import { useNavigate } from "react-router-dom";
+
+// const ProfileButton = ({user}) => {
+//     const dispatch =  useDispatch();
+//     const navigate = useNavigate();
+//     const [showMenu, setShowMenu] = useState(false);
+//     const ulRef = useRef(); // Ref for the dropdown menu
+
+//     const toggleMenu = (e) => {
+//         e.stopPropagation();
+//         setShowMenu(!showMenu); // Toggle the menu visibility
+//     };
+
+//     useEffect(() => {
+//         if (!showMenu) return; // If the menu is not shown, do nothing
+
+//         // Function to close the menu if a click is detected outside of it
+//         const closeMenu = (e) => {
+//             if (ulRef.current && !ulRef.current.contains(e.target)) {
+//                 setShowMenu(false); // Close the menu
+//             }
+//         };
+
+//         document.addEventListener('click', closeMenu);
+
+//         // Cleanup the event listener when the component is unmounted or the menu is closed
+//         return () => document.removeEventListener('click', closeMenu);
+//     }, [showMenu]);
+
+//     const closeMenuManually = () => setShowMenu(false); // This can be passed as a prop to close the menu manually
+
+//     const logoutClick = (e) => {
+//         e.preventDefault();
+//         dispatch(logout());
+//         closeMenuManually();
+//         navigate('/');
+//     };
+
+//     const dropdownClasses = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+//     return (
+//         <div>
+//             <button id="toggle-menu-button" onClick={toggleMenu}>
+//                 <IoMenu />
+//                 <FaCircleUser />
+//             </button>
+//             <ul className={dropdownClasses} ref={ulRef}> {/* Attach the ref to the dropdown menu */}
+//                 {user ? (
+//                     <>
+//                         <li>Hello, {user.firstName}</li>
+//                         <li>{user.email}</li>
+//                         <hr></hr>
+//                         <li onClick={() => navigate('/spots/current')}>Manage Spots</li>
+//                         <hr></hr>
+//                         <li id="logout">
+//                             <button onClick={logoutClick}>Log Out</button>
+//                         </li>
+//                     </>
+//                 ) : (
+//                     <>
+//                         <OpenModalMenuItem
+//                             itemText="Sign Up"
+//                             onItemClick={closeMenuManually} // Close menu after clicking
+//                             modalComponent={<SignupFormModal />}
+//                         />
+//                         <OpenModalMenuItem
+//                             itemText="Log In"
+//                             onItemClick={closeMenuManually} // Close menu after clicking
+//                             modalComponent={<LoginFormModal />}
+//                         />
+//                     </>
+//                 )}
+//             </ul>
+//         </div>
+//     );
+// }
+
+// export default ProfileButton;
--- frontend/src/context/Modal.jsx
+++ frontend/src/context/Modal.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { useRef, createContext, useState, useContext } from 'react';
 import ReactDOM from 'react-dom';
-import './Modal.css'
+import './Modal.css';
 
 const ModalContext = createContext();
 
-export const ModalProvider = ({children}) => {
-    const modalRef = useRef()
+export const ModalProvider = ({ children }) => {
+    const modalRef = useRef();
     const [modalContent, setModalContent] = useState(null);
-    const [onModalClose, setOnModalClose] = useState(null)
+    const [onModalClose, setOnModalClose] = useState(null);
 
     const closeModal = () => {
         setModalContent(null);
         if (typeof onModalClose === 'function') {
             setOnModalClose(null);
-            onModalClose()
+            onModalClose();
         }
-    }
+    };
 
     const contextValue = {
         modalRef,
@@ -24,7 +24,7 @@ export const ModalProvider = ({children}) => {
         setModalContent,
         setOnModalClose,
         closeModal
-    }
+    };
 
     return (
         <>
@@ -33,12 +33,14 @@ export const ModalProvider = ({children}) => {
             </ModalContext.Provider>
             <div ref={modalRef} />
         </>
-    )
-}
+    );
+};
 
 export const Modal = () => {
     const { modalRef, modalContent, closeModal } = useContext(ModalContext);
     console.log('Modal content:', modalContent);
+    console.log('Modal ref:', modalRef.current); // This should log a DOM element, not null
+
     if (!modalRef || !modalRef.current || !modalContent) return null;
 
     return ReactDOM.createPortal(
@@ -47,7 +49,65 @@ export const Modal = () => {
             <div id='modal-content'>{modalContent}</div>
         </div>,
         modalRef.current
-    )
-}
+    );
+};
 
 export const useModal = () => useContext(ModalContext);
+
+
+
+
+
+// import React from 'react';
+// import { useRef, createContext, useState, useContext } from 'react';
+// import ReactDOM from 'react-dom';
+// import './Modal.css'
+
+// const ModalContext = createContext();
+
+// export const ModalProvider = ({children}) => {
+//     const modalRef = useRef()
+//     const [modalContent, setModalContent] = useState(null);
+//     const [onModalClose, setOnModalClose] = useState(null)
+
+//     const closeModal = () => {
+//         setModalContent(null);
+//         if (typeof onModalClose === 'function') {
+//             setOnModalClose(null);
+//             onModalClose()
+//         }
+//     }
+
+//     const contextValue = {
+//         modalRef,
+//         modalContent,
+//         setModalContent,
+//         setOnModalClose,
+//         closeModal
+//     }
+
+//     return (
+//         <>
+//             <ModalContext.Provider value={contextValue}>
+//                 {children}
+//             </ModalContext.Provider>
+//             <div ref={modalRef} />
+//         </>
+//     )
+// }
+
+// export const Modal = () => {
+//     const { modalRef, modalContent, closeModal } = useContext(ModalContext);
+//     console.log('Modal content:', modalContent);
+//     if (!modalRef || !modalRef.current || !modalContent) return null;
+
+//     return ReactDOM.createPortal(
+//         <div id='modal'>
+//             <div id='modal-background' onClick={closeModal} />
+//             <div id='modal-content'>{modalContent}</div>
+//         </div>,
+//         modalRef.current
+//     )
+// }
+
+// export const useModal = () => useContext(ModalContext);
