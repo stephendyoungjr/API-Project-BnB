1ba76b404e23f20356c6dbb33eb463df9e0d06ac
stephendyoungjr <stephenyoung.np@outlook.com>
2024-09-02 18:10:01 -0400

    nav

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -18,7 +18,6 @@ const Navigation = ({isLoaded}) => {
         <ul>
             <li id="home-nav">
                 <NavLink to="/">
-                <link rel="icon" type="image" href="./iconimage/warbnblogo.jpeg" />
                     Warbnb
                 </NavLink>
             </li>
@@ -36,47 +35,4 @@ const Navigation = ({isLoaded}) => {
 };
 
 export default Navigation;
-
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import { NavLink, useNavigate } from "react-router-dom";
-// import ProfileButton from "./ProfileButton";
-// import './Navigation.css';
-
-// const Navigation = ({ isLoaded }) => {
-//     const sessionUser = useSelector(state => state.session.user);
-//     const navigate = useNavigate();
-
-//     const handleClick = (e) => {
-//         e.preventDefault();
-//         navigate('/spots/new');
-//     };
-
-//     return (
-//         <nav>
-//             <ul>
-//                 <li id="home-nav">
-//                     <NavLink to="/">
-//                         <div id="site-icon">
-//                             <img src="/warbnblogo.jpeg" alt="Warbnb Logo" id="logo-image" />
-//                         </div>
-//                         Warbnb
-//                     </NavLink>
-//                 </li>
-//                 {isLoaded && (
-//                     <div id="user-nav">
-//                         <li className={sessionUser ? '' : 'hide'} onClick={handleClick}>Create a New Spot</li>
-//                         <li>
-//                             <ProfileButton user={sessionUser} />
-//                         </li>
-//                     </div>
-//                 )}
-//             </ul>
-//         </nav>
-//     );
-// };
-
-// export default Navigation;
-
-
 
