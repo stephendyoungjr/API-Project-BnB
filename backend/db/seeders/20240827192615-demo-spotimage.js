312c0bcc384f518ee42c81584a5edd0dfe3a640b
stephendyoungjr <stephenyoung.np@outlook.com>
2024-09-11 19:17:32 -0400

    added more spot photos

--- backend/db/seeders/20240827192615-demo-spotimage.js
+++ backend/db/seeders/20240827192615-demo-spotimage.js
@@ -18,9 +18,29 @@ const demoSpotImages = [
     url: "/warbnb2.jpeg"
   },
   {
+    spotId: 1,
+    url: "/warbnb18.jpeg"
+  },
+  {
+    spotId: 1,
+    url: "/warbnb19.jpeg"
+  },
+  {
+    spotId: 1,
+    url: "/warbnb20.jpeg"
+  },
+  {
     spotId: 7,
     url: "/warbnb3.jpeg",
     preview: true
+  },
+  {
+    spotId: 7,
+    url: "/warbnb29.jpeg"
+  },
+  {
+    spotId: 7,
+    url: "/warbnb30.jpeg"
   },
   {
     spotId: 7,
@@ -34,6 +54,18 @@ const demoSpotImages = [
     spotId: 6,
     url: "/warbnb6.jpeg",
     preview: true
+  },
+  {
+    spotId: 6,
+    url: "/warbnb23.jpeg",
+  },
+  {
+    spotId: 6,
+    url: "/warbnb24.jpeg",
+  },
+  {
+    spotId: 6,
+    url: "/warbnb25.jpeg",
   },
   {
     spotId: 6,
@@ -53,6 +85,14 @@ const demoSpotImages = [
     url: "/warbnb10.jpeg"
   },
   {
+    spotId: 8,
+    url: "/warbnb21.jpeg"
+  },
+  {
+    spotId: 8,
+    url: "/warbnb22.jpeg"
+  },
+  {
     spotId: 2,
     url: "/warbnb11.jpeg",
     preview: true
@@ -60,6 +100,18 @@ const demoSpotImages = [
   {
     spotId: 2,
     url: "/warbnb12.jpeg"
+  },
+  {
+    spotId: 2,
+    url: "/warbnb35.jpeg"
+  },
+  {
+    spotId: 2,
+    url: "/warbnb27.jpeg"
+  },
+  {
+    spotId: 2,
+    url: "/warbnb28.jpeg"
   },
   {
     spotId: 3,
@@ -71,6 +123,18 @@ const demoSpotImages = [
     url: "/warbnb14.jpeg"
   },
   {
+    spotId: 3,
+    url: "/warbnb29.jpeg"
+  },
+  {
+    spotId: 3,
+    url: "/warbnb30.jpeg"
+  },
+  {
+    spotId: 3,
+    url: "/warbnb45.jpeg"
+  },
+  {
     spotId: 4,
     url: "/warbnb15.jpeg",
     preview: true
@@ -80,10 +144,44 @@ const demoSpotImages = [
     url: "/warbnb16.jpeg"
   },
   {
+    spotId: 4,
+    url: "/warbnb41.jpeg"
+  },
+  {
+    spotId: 4,
+    url: "/warbnb42.jpeg"
+  },
+  {
+    spotId: 4,
+    url: "/warbnb43.jpeg"
+  },
+  {
     spotId: 5,
     url: "/warbnb17.jpeg",
     preview: true
-  }
+  },
+  {
+    spotId: 5,
+    url: "/warbnb31.jpeg",
+  },
+  {
+    spotId: 5,
+    url: "/warbnb32.jpeg",
+  },
+  {
+    spotId: 5,
+    url: "/warbnb33.jpeg",
+  },
+  {
+    spotId: 5,
+    url: "/warbnb34.jpeg",
+  },
+  
+  
+  
+  
+
+
 ];
 
 /** @type {import('sequelize-cli').Migration} */
